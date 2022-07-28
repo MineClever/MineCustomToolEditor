fb4e94c385ebc1557f5cb26c69d81d56a6ba997b
unknown <zhengzhe@luheli.com>
2022-07-28 18:20:33 +0800

    # Fast push

--- Source/MineCustomToolEditor/Private/SequencerExtension/SequencerBaseMenuAction.cpp
+++ Source/MineCustomToolEditor/Private/SequencerExtension/SequencerBaseMenuAction.cpp
@@ -134,13 +134,14 @@ namespace FMineSequencerBaseMenuAction_Helper_Internal
 
                             // Find ABC Path
                             TArray<FString> AbcPathArray;
-                            // BUG: need a method found correct Package path
+
+                            // TODO: need a method found correct Package path
+                            
                             MakeRelativeAbcDirPath("/Game/PalTrailer/Characters/Nanzhu/Rig_Nanzhu.Rig_Nanzhu", AbcPathArray);
 
                             for (auto SlotName : SlotNames) {
                                 bool &&HasProxyTag = false;
 
-                                // TODO: Switch to PathFinder!
                                 FString MatchedPackagePath;
                                 for (auto AbcDirPath : AbcPathArray) {
                                     UE_LOG (LogMineCustomToolEditor, Log, TEXT ("Searching @ %s"), *AbcDirPath);
